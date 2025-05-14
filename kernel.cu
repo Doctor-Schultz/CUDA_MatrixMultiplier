3a4e7b1415fdab7401e3d74a420d40b49e713f39
Doctor-Schultz <114769197+Doctor-Schultz@users.noreply.github.com>
2025-05-14 12:18:41 -0700

    Generic visual studio code
    Code auto generated by visual studio, all of this will be removed in future pushes

--- kernel.cu
+++ kernel.cu
@@ -8,6 +8,7 @@ cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
 
 __global__ void addKernel(int *c, const int *a, const int *b)
 {
+    // test comment
     int i = threadIdx.x;
     c[i] = a[i] + b[i];
 }
